46c7efc9e662e05a0796b1842380e89b011cbb3a
w4lidi <walidiwahyupratama@gmail.com>
2023-12-08 20:37:11 +0800

    error fixing

--- app/src/main/java/com/lembain/walidirecycleview/DetailBuku.kt
+++ app/src/main/java/com/lembain/walidirecycleview/DetailBuku.kt
@@ -1,5 +1,6 @@
 package com.lembain.walidirecycleview
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,15 +19,16 @@ class DetailBuku() : AppCompatActivity() {
         val judul : TextView = findViewById(R.id.textJudul)
         val decs : TextView = findViewById(R.id.textDetail)
 
-        val objRecv = intent.getParcelableExtra(EXTRA_NAME, Book::class.java)
+//        val objRecv = Intent.getParcelableExtra(EXTRA_NAME, Book::class.java)
+        val objRecv = intent.getParcelableExtra<Book>("extra_name")
 
         Glide.with(this).load(objRecv?.photo).into(img)
 
-        judul.text = objRecv?.name
-        decs.text = objRecv?.description
+        judul.text = objRecv?.name.toString()
+        decs.text = objRecv?.description.toString()
     }
 
     companion object {
-        val EXTRA_NAME: String? = "extra_data"
+        val EXTRA_NAME: String = "extra_name"
     }
 }
